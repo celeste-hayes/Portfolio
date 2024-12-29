e637f89111bc814ea16e9552eb199f4137046ba8
Celeste Hayes <hayes.celeste93@gmail.com>
2024-12-29 09:10:36 -0500

    feat: add contact page information

---
+++ React Portfolio/src/pages/contact.jsx
@@ -0,0 +1,10 @@
+export default function Contact() {
+    return (
+      <div>
+        <h1>Contact Me</h1>
+        <p>
+        Details about my contact info go here.
+        </p>
+      </div>
+    );
+  }
