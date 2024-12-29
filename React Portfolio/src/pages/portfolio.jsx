ae580e11671696b3dc3794c071c4afc8ca0db8b3
Celeste Hayes <hayes.celeste93@gmail.com>
2024-12-29 12:27:57 -0500

    feat: add portfolio page

---
+++ React Portfolio/src/pages/portfolio.jsx
@@ -0,0 +1,10 @@
+export default function Portfolio() {
+    return (
+      <div>
+        <h1>Portfolio</h1>
+        <p>
+          Details about portfolio go here.
+        </p>
+      </div>
+    );
+  }
