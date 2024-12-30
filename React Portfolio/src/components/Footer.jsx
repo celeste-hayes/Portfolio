f764698ebd17d011a9a9b530d6742e65b6df839a
Celeste Hayes <hayes.celeste93@gmail.com>
2024-12-29 20:14:14 -0500

    refactor: add style adjustments for footer to app.css

--- React Portfolio/src/components/Footer.jsx
+++ React Portfolio/src/components/Footer.jsx
@@ -5,10 +5,10 @@ export default function Footer() {
         <footer className="py-3" style={{ position: 'fixed', bottom: 0, left: 0, width: '100%', zIndex: 1000 }}>
             <div className="container d-flex justify-content-center">
                 <a href="https://github.com/celeste-hayes" target="_blank" className="mx-2 noopener noreferrer">
-                    <FaGithub size={25} />
+                    <FaGithub size={35} className='icon-color'/>
                 </a>
                 <a href="https://www.linkedin.com/in/celeste-hayes-a93464b8/" target="_blank" className="mx-2 noopener noreferrer">
-                    <FaLinkedin size={25} />
+                    <FaLinkedin size={35} className='icon-color'/>
                 </a>
             </div>
         </footer>
