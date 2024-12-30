6b3193108dfb38946e6d725db3939ee270625b7d
Celeste Hayes <hayes.celeste93@gmail.com>
2024-12-29 21:04:15 -0500

    refactor: add contact page content in place of placeholder

--- React Portfolio/src/pages/contact.jsx
+++ React Portfolio/src/pages/contact.jsx
@@ -1,10 +1,27 @@
 export default function Contact() {
-    return (
-      <div>
-        <h1>Contact Me</h1>
-        <p>
-        Details about my contact info go here.
-        </p>
-      </div>
-    );
+
+  const handleSubmit = (e) => {
+      e.preventDefault();
+      const formData = new FormData(e.target);
+      const data = Object.fromEntries(formData.entries());
+      //TODO: send data to server
+      console.log(data);
   }
+
+  return (
+      <div>
+          <h2 className="display-1 fw-normal mb-4">contact.</h2>
+          <p className= "fs-2">Let's get connected!</p>
+          <form className="contact-form" style={{display:'flex', flexDirection:'column', width: '500px'}} onSubmit={handleSubmit}>
+              <label className="fs-5" htmlFor="name">Name:</label>
+              <input type="text" id="name" name="name" className="mb-2 custom-input" required />
+              <label className="fs-5" htmlFor="email">Email:</label>
+              <input type="email" id="email" name="email" className="mb-2 custom-input" required />
+              <label className="fs-5" htmlFor="message">Message:</label>
+              <textarea id="message" name="message" className="mb-2 custom-input" required style={{height: '200px'}}>
+              </textarea>
+              <button type="send" className="fs-5 button">Send</button>
+          </form>
+      </div>
+  )
+}
